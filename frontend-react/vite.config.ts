a5e7329dbcc13ea07de5b6440429ca28062007e3
Ahsin <24550005@gm.uit.edu.vn>
2025-08-30 15:25:43 +0700

    Add base react

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/K24-Learning_Management_System/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 5000
